de4ab6b72424db40a5f22e8ef1b1d0bdbd797e9d
Trist1998 <tristanmwood@hotmail.com>
2019-03-21 17:58:25 +0200

    Loadshedding save

---
+++ Assets/RaceManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour
+{
+    public int lap;
+    public int currentSectionId;
+
+    public void enterNextSection(int sectionId, int nextExpectedId)
+    {
+        if(sectionId == expectedNextSectionId)
+        {
+            currentSectionId = sectionId;
+            expectedNextSectionId = nextExpectedId;
+        }
+    }
+
+    internal void enterStartSection(int sectionId, int nextId)
+    {
+        if (sectionId == expectedNextSectionId)
+        {
+            currentSectionId = sectionId;
+            expectedNextSectionId = nextId;
+            lap++;
+        }
+    }
+}
---
+++ Assets/Scripts/Level/TrackSection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackSection : MonoBehaviour
+{
+    private const string TAG_PLAYER = "Player";
+    public int sectionId;
+    public int nextId;
+    public bool startLine = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == TAG_PLAYER)
+        {
+            RaceManager raceManager = other.gameObject.GetComponent<RaceManager>();
+            if (!startLine)
+                raceManager.enterNextSection(sectionId, nextId);
+            else
+                raceManager.enterStartSection(sectionId, nextId);
+        }
+    }
+}
